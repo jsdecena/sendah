ae07928e997e774a69e35797bb81d8c0efa557f2
Jeff Simons Decena <jeff.decena@yahoo.com>
2015-06-05 18:34:22 +0800

    initial files

---
+++ assets/js/default.js
@@ -0,0 +1,50 @@
+$(document).ready(function(){
+    
+    var ddElement = '.top-newsletter-dropdown';
+
+    //CHECK FOR THE COOKIE BEFORE LOADING THE POP UP AGAIN
+    if ($.cookie('mchimp-pop') != 'loaded'){
+        //CHECK THE WINDOW WIDTH IF SMALL DEVICE OR WEB
+        if ($(window).width() > 640) {
+            //ON THE PAGE LOAD, REVEAL THE NEWSLETTER TOP
+            $(ddElement).show().animate({'margin-top' : 0});
+        }else{
+            //ON THE PAGE LOAD, REVEAL THE NEWSLETTER TOP
+            $(ddElement).show().animate({'margin-top' : 51});
+        };
+    }
+
+    //WHEN THE USER CLICKS THE CLOSE BUTTON, ANIMATE HIDE THE NEWSLETTER TOP
+    $('#btn-close-newsletter').on('click', function(){
+        $(ddElement).animate({'margin-top' : -126 }, function(){
+            $.cookie('mchimp-pop', 'loaded');
+        });
+    });
+
+    /*MENU*/
+    $('#nav-expander').on('click',function(e){
+        e.preventDefault();
+        $('body').toggleClass('nav-expanded');
+    });
+    $('#nav-close').on('click',function(e){
+        e.preventDefault();
+        $('body').removeClass('nav-expanded');
+    });
+
+    // Initialize navgoco with default options
+    $(".main-menu").navgoco({
+        caret: '<span class="caret"></span>',
+        accordion: false,
+        openClass: 'open',
+        save: true,
+        cookie: {
+            name: 'navgoco',
+            expires: false,
+            path: '/'
+        },
+        slide: {
+            duration: 300,
+            easing: 'swing'
+        }
+    });
+});
